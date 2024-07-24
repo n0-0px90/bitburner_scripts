d88113b451800e434ecbcacbfb9213dc73864a10
n0-0px90 <cameron.nichols415@gmail.com>
2024-07-24 19:38:02 -0400

    Optimized a few things between roots and calc

--- auto_roots.js
+++ auto_roots.js
@@ -2,40 +2,62 @@
 export async function main(ns) {
   const my_level = ns.getHackingLevel()
   const files = ["auto_roots.js", "calc_ram.js", "money_print.js", "kill_all.js"]
+  let target = ns.args[0];
   let get_neighbors = ns.scan();
   get_neighbors = get_neighbors.slice(0, 7);
   for (let host of get_neighbors) {
     let required_ports = ns.getServerNumPortsRequired(host)
     let hacking_level = ns.getServerRequiredHackingLevel(host)
-    await nuke_hosts(ns, host, required_ports, hacking_level, my_level)
-    ns.scp(files, host, "home")
-    if(ns.isRunning("auto_roots.js", host) == false){
-      ns.exec("auto_roots.js", host);
-    }
+    nuke_hosts(ns, host, required_ports, hacking_level, my_level)
+    copy_run_kill(ns, target, host, files)
     await ns.sleep(500);
   }
-  await ns.sleep(10000);
-  ns.scriptKill("auto_roots.js", ns.getHostname())
+  await ns.sleep(500);
+  ns.spawn("calc_ram.js", { threads: 1, spawnDelay: 10000 }, target);
 }
+
+/**@param {NS} ns Scripts needed to work
+ * @param {String} target Arguement passed from cli to script
+ * @param {String} host The current host being worked on
+ * @param {String[]} files The files being copied over
+ */
+async function copy_run_kill(ns, target, host, files) {
+  ns.scp(files, host, "home")
+  let host_processes = ns.ps(host);
+  if (host_processes.length == 0) {
+    ns.exec("auto_roots.js", host, 1, target)
+  } else {
+    for (let process of host_processes) {
+      var script_name = process.filename
+      if (script_name != "auto_roots.js") {
+        ns.kill(process.pid);
+        ns.exec("auto_roots.js", host, 1, target)
+      }
+    }
+  }
+}
+
+
+
 /** @param {NS} ns  Collection of all functions passed to script
  *  @param {String} host  the host that you are trying to hack
  *  @param {Number} required_ports  required ports that they need to be hacked
  *  @param {Number} hacking_level  the server's hack level
  *  @param {Number} my_level  your hack level
 */
-export async function nuke_hosts(ns, host, required_ports, hacking_level, my_level) {
+async function nuke_hosts(ns, host, required_ports, hacking_level, my_level) {
   var hacking_progs = [ns.brutessh, ns.ftpcrack, ns.relaysmtp, ns.httpworm, ns.sqlinject];
-  for(var ports=0; ports<=required_ports; ports++){
-    try{
-    if(required_ports == 0){
-      ns.nuke(host);
-    } else {
-      var script = hacking_progs[ports];
-      script(host);
-      ns.nuke(host);
-    }
+  for (var ports = 0; ports <= required_ports; ports++) {
+    try {
+      if (required_ports == 0) {
+        ns.nuke(host);
+      } else {
+        var script = hacking_progs[ports];
+        script(host);
+        ns.nuke(host);
+      }
     } catch (e) {
       ns.print(e);
-    }    
+    }
   }
 }
--- calc_ram.js
+++ calc_ram.js
@@ -1,20 +1,13 @@
 /** @param {NS} ns */
 export async function main(ns) {
+  let target = ns.args[0];
   let svhostname = ns.getHostname();
-  let hosts = ns.scan();
-  hosts = hosts.slice(0, 7);
-  for (var host of hosts) {
-    if(ns.isRunning("calc_ram.js", host) == false){
-      ns.exec("calc_ram.js", host);  
-    }
-    await ns.sleep(2000);
-  }
   let server_ram = Math.round(ns.getServerMaxRam(svhostname));
   let execthreads = await calc_ram(server_ram);
-  await ns.sleep(3000);
-  let set_max_money = ns.getServerMaxMoney(svhostname) * .75;
-  let set_min_sec = ns.getServerMinSecurityLevel(svhostname) + 3;
-  ns.spawn("money_print.js", {threads: execthreads, spawnDelay: 100}, svhostname, set_max_money, set_min_sec);
+  await ns.sleep(5000);
+  let set_max_money = Math.ceil(ns.getServerMaxMoney(target) * .75);
+  let set_min_sec = ns.getServerMinSecurityLevel(target) + 3;
+  ns.spawn("money_print.js", {threads: execthreads, spawnDelay: 100}, target, set_max_money, set_min_sec);
 }
 
 /** Calculate how many threads you can use to run money_print.js 
@@ -22,7 +15,7 @@ export async function main(ns) {
  * @returns The threads the server can use
  */
 async function calc_ram(max_ram){
-  let threads = Math.floor(max_ram / 3)
+  let threads = Math.floor(max_ram / 2.2)
   if(threads <= 1){
     return threads=1;
   }
