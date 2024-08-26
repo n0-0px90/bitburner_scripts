fb5c3eb7bc570f0e23f996878a66e6b579108e79
n0-0px90 <cameron.nichols415@gmail.com>
2024-08-26 00:46:06 -0400

    Added BFS algo into auto_roots.js

--- early_game/auto_roots.js
+++ early_game/auto_roots.js
@@ -1,20 +1,48 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  const my_level = ns.getHackingLevel()
-  const files = ["auto_roots.js", "calc_ram.js", "money_print.js", "kill_all.js"]
+  const files = ["auto_roots.js", "calc_ram.js", "money_print.js"];
   let target = ns.args[0];
-  let get_neighbors = ns.scan();
-  get_neighbors = get_neighbors.slice(0, 7);
+  let saStartNode = ['home'];
+  let get_neighbors = await get_nodes(ns, saStartNode);
   for (let host of get_neighbors) {
     let required_ports = ns.getServerNumPortsRequired(host)
-    let hacking_level = ns.getServerRequiredHackingLevel(host)
-    nuke_hosts(ns, host, required_ports, hacking_level, my_level)
+    nuke_hosts(ns, host, required_ports)
     copy_run_kill(ns, target, host, files)
-    await ns.sleep(500);
+    await ns.sleep(250);
   }
-  await ns.sleep(500);
+  await ns.sleep(1000);
   ns.spawn("calc_ram.js", { threads: 1, spawnDelay: 10000 }, target);
 }
+
+/** @param {NS} ns Scripts needed to work
+ * @param {String[]} saStartNode The starting node to where you're at
+ */
+//saVisited is the start array, holds home.\
+//home is scanned, array returned into saNode0
+//returns first element of array to currentnode, checks to see if current node is in visited.
+//if not in visited, push current node into visited, then were going to scan current node
+//saFollowonNodes holds return for sCurrentNode scan.
+//iterates through saFollowonNodes, if current node does not exist in saVisited, push to visited.
+//Returns saVisisted
+//NOTE this may jack something up later on
+async function get_nodes(ns, saStartNode){
+  let saVisited = saStartNode;
+  let saNode0 = ns.scan('home');
+  while (saNode0.length > 0){
+    let sCurrentNode = saNode0.shift();
+    if (saVisited.includes(sCurrentNode) == false){
+      saVisited.push(sCurrentNode);
+      let saFollowonNodes = ns.scan(sCurrentNode);
+      for (let i = 0; i < saFollowonNodes.length; i++){
+        if (saVisited.includes(saFollowonNodes[i]) == false){
+          saNode0.push(saFollowonNodes[i]);
+        }
+      }
+    }
+  }
+  return saVisited;
+}
+
 
 /**@param {NS} ns Scripts needed to work
  * @param {String} target Arguement passed from cli to script
@@ -42,10 +70,8 @@ async function copy_run_kill(ns, target, host, files) {
 /** @param {NS} ns  Collection of all functions passed to script
  *  @param {String} host  the host that you are trying to hack
  *  @param {Number} required_ports  required ports that they need to be hacked
- *  @param {Number} hacking_level  the server's hack level
- *  @param {Number} my_level  your hack level
 */
-async function nuke_hosts(ns, host, required_ports, hacking_level, my_level) {
+async function nuke_hosts(ns, host, required_ports) {
   var hacking_progs = [ns.brutessh, ns.ftpcrack, ns.relaysmtp, ns.httpworm, ns.sqlinject];
   for (var ports = 0; ports <= required_ports; ports++) {
     try {
